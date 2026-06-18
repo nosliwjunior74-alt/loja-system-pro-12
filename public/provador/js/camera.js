a95f6c5c1375d61424d185b32ddd690f6997e9fb
nosliwjunior74-alt <nosliwjunior74@gmail.com>
2026-06-17 23:32:38 -0300

    Update camera.js

--- public/provador/js/camera.js
+++ public/provador/js/camera.js
@@ -50,11 +50,25 @@ window.CameraModule={
         canvas.width=video.videoWidth; canvas.height=video.videoHeight;
         ctx.clearRect(0,0,canvas.width,canvas.height);
         ctx.save(); ctx.scale(-1,1); ctx.drawImage(video,-canvas.width,0,canvas.width,canvas.height); ctx.restore();
-        if(this.overlayImg&&this.overlayImg.complete){
-          const rect={x:canvas.width*0.29,y:canvas.height*0.18,w:canvas.width*0.42,h:canvas.height*0.46};
-          console.log('DESENHANDO LOOK', rect);
-          ctx.drawImage(this.overlayImg,rect.x,rect.y,rect.w,rect.h);
-        }
+       if(this.overlayImg && this.overlayImg.complete){
+
+  const rect = this.lastRect || {
+    x: canvas.width * 0.29,
+    y: canvas.height * 0.18,
+    w: canvas.width * 0.42,
+    h: canvas.height * 0.46
+  };
+
+  console.log('DESENHANDO LOOK', rect);
+
+  ctx.drawImage(
+    this.overlayImg,
+    rect.x,
+    rect.y,
+    rect.w,
+    rect.h
+  );
+}
       }
       requestAnimationFrame(draw);
     }; draw();
